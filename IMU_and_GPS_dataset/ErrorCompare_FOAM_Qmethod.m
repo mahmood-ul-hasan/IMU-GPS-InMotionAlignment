ff3d84da6fc83f2464d69c875b58ec9ab6b4c0b0
mahmood hassan <95124439+mahmood-ul-hasan@users.noreply.github.com>
2024-05-10 20:35:20 +0900

    upload

---
+++ IMU_and_GPS_dataset/ErrorCompare_FOAM_Qmethod.m
@@ -0,0 +1,28 @@
+ResultData=importdata('ResultData.txt');
+ResultDataFilter=importdata('ResultDataFilter.txt');
+
+% ResultData=[angle_Qbn*180/pi angle_qbn_Ref];
+
+angle_Qbn=ResultData(:,1:3);
+angle_qbn=ResultData(:,4:6);
+Error=ResultData(:,7:9);
+
+
+angle_Qbn_Filter=ResultDataFilter(:,1:3);
+angle_qbn_Ref_Filter=ResultDataFilter(:,4:6);
+Error_Filter=ResultDataFilter(:,7:9);
+
+figure, plot(TimeAxisGPS,Error_quat,'LineWidth',2), title('Error in degree'); grid on, xlabel('Time /s'); ylabel('Error using quat in degree'),legend('Roll','Pitch','yaw'), %axis([0,t*nn_gps,-0.05,0.05])
+
+size(Error)
+size(Error_Filter)
+size(TimeAxisGPS)
+
+figure, 
+subplot(3,1,1), plot(TimeAxisGPS,Error(:,1),'LineWidth',2), hold on, plot(TimeAxisGPS,Error_Filter(:,1),'r','LineWidth',2), grid on,  ylabel('Roll Error (deg)'), legend( 'Traditional OBA' , 'Proposed Method')%axis([0,t*nn_gps,-0.05,0.5])
+subplot(3,1,2), plot(TimeAxisGPS,Error(:,2),'LineWidth',2), hold on, plot(TimeAxisGPS,Error_Filter(:,2),'r','LineWidth',2), grid on,  ylabel('Pitch Error (deg)')
+subplot(3,1,3), plot(TimeAxisGPS,Error(:,3),'LineWidth',2), hold on, plot(TimeAxisGPS,Error_Filter(:,3),'r','LineWidth',2), grid on,  xlabel('Time /s'); ylabel('Yaw Error (deg)')
+
+
+figure, plot(TimeAxisGPS,angle_qbn_Ref(:,3),'g','LineWidth',2), hold on, plot(TimeAxisGPS,angle_Qbn_Filter(:,3),'r','LineWidth',2), hold on, plot(TimeAxisGPS,angle_Qbn(:,3),'b','LineWidth',2), 
+hold on,grid on, xlabel('Time /s'); ylabel('Yaw angle (deg)'), legend( 'True Attitude','Estimated by Proposed Method', 'Estimated by Traditional OBA' )
